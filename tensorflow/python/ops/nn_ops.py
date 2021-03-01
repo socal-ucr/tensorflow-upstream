fd6c08a0d267b28613d96631d844169bac28e5b9
Deven Desai <deven.desai.amd@gmail.com>
2021-03-01 16:22:12 +0000

    ROCm specific Gelu

--- tensorflow/python/ops/nn_ops.py
+++ tensorflow/python/ops/nn_ops.py
@@ -3705,7 +3705,10 @@ def gelu(features, approximate=False, name=None):
   with ops.name_scope(name, "Gelu", [features]):
     features = ops.convert_to_tensor(features, name="features")
     if approximate:
-      return gen_nn_ops.gelu(features, name=name)
+      coeff = math_ops.cast(0.044715, features.dtype)
+      return 0.5 * features * (
+          1.0 + math_ops.tanh(0.7978845608028654 *
+                              (features + coeff * math_ops.pow(features, 3))))
     else:
       return 0.5 * features * (1.0 + math_ops.erf(
           features / math_ops.cast(1.4142135623730951, features.dtype)))
