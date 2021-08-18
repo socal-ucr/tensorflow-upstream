a9bdda7f4c200619d5fb8d15f7c58b50d29f4b30
Steven I Reeves <steven.reeves@amd.com>
2021-08-18 01:44:47 +0000

    dist_packages not site_packages.

--- tensorflow/tools/ci_build/install/install_pi_python3.9_toolchain.sh
+++ tensorflow/tools/ci_build/install/install_pi_python3.9_toolchain.sh
@@ -21,4 +21,3 @@ apt-get install -y python3-pip
 ln -sf /usr/bin/python3.9 /usr/local/bin/python3.9
 apt-get install -y python3.9-distutils
 python3.9 -m pip install --upgrade pip
-ln -sf /usr/local/lib/python3.9/dist-packages/numpy/core/include/numpy /usr/include/python3.9/numpy
